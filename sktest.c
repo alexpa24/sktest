c62eef4668c2bf8e0b1a3f09f42260feec5befe8
Alex Palomo <apapuz@gmail.com>
2018-07-21 11:37:46 +0100

    minor changes

--- sktest.c
+++ sktest.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
         {
             if (strcmp(argv[i], "-n") == 0) //number of HTTP requests to make
             {   
-                n = atoi(argv[i+1]);   
+                n = atoi(argv[i+1]); 
                 i++;
             }
             if (strcmp(argv[i], "-H") == 0) //HTTP header information
